3946dbfe6b587fd2716bb0f0fb6f174be8400b5d
Evshaddock <evshaddock@gmail.com>
2018-08-09 03:07:18 -0500

    Unbroke the things I just broke

--- PhotoOrg/Form1.cs
+++ PhotoOrg/Form1.cs
@@ -19,8 +19,10 @@ namespace PhotoOrg
             {
                 string[] log = File.ReadAllLines("jobs.txt");
                 foreach (string file in log)
-                {checkedListBox1.Items.Add(log[count]);
-                 count++;}
+                {
+                    checkedListBox1.Items.Add(log[count]);
+                    count++;
+                }
             }
 
             // Check for the header file, if it's there, change the header to what's in there.
@@ -32,7 +34,7 @@ namespace PhotoOrg
             }
         }
 
-            // Magic 'grab the window anywhere to drag' code I nabbed. Don't know how it works, but it does. 
+        // Magic 'grab the window anywhere to drag' code I nabbed. Don't know how it works, but it does. 
 
         protected override void WndProc(ref Message m)
         {
@@ -45,7 +47,7 @@ namespace PhotoOrg
         private const int HT_CLIENT = 0x1;
         private const int HT_CAPTION = 0x2;
 
-            // Pressing the 'Save' button
+        // Pressing the 'Save' button
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -58,8 +60,8 @@ namespace PhotoOrg
             {
                 if (!Directory.Exists(order.Text + " - " + name.Text))
                     Directory.CreateDirectory(order.Text + " - " + name.Text);
-                if (!File.Exists(order.Text + " - " + name.Text + "/" + order.Text + " - " + name.Text + " - " + ( files + 1 ) + ".jpg"))
-                    File.Copy(openFileDialog1.FileNames[files], order.Text + " - " + name.Text + "/" +  name.Text + " - " + ( files + 1 ) + ".jpg");
+                if (!File.Exists(order.Text + " - " + name.Text + "/" + order.Text + " - " + name.Text + " - " + (files + 1) + ".jpg"))
+                    File.Copy(openFileDialog1.FileNames[files], order.Text + " - " + name.Text + "/" + name.Text + " - " + (files + 1) + ".jpg");
                 files++;
             }
 
@@ -68,7 +70,7 @@ namespace PhotoOrg
             string job = "Job #" + order.Text + " for " + name.Text + " : " + files + " files";
             List<string> jobs = new List<string>();
             jobs.Add(job);
-            string[] info = { "Name: " + name.Text, "Phone: " + phone.Text, "Order #: " + order.Text, "E-Mail: " + email.Text, "Address: " + address.Text};
+            string[] info = { "Name: " + name.Text, "Phone: " + phone.Text, "Order #: " + order.Text, "E-Mail: " + email.Text, "Address: " + address.Text };
             File.WriteAllLines(order.Text + " - " + name.Text + "/" + "Order info for " + " " + name.Text + ".txt", info);
             jobs.ToArray();
             checkedListBox1.Items.Add(job);
@@ -88,27 +90,27 @@ namespace PhotoOrg
             save.Enabled = false;
         }
 
-        private void logo_Click(object sender, EventArgs e){timer1.Start();}
+        private void logo_Click(object sender, EventArgs e) { timer1.Start(); }
 
-            // Stuff to show the 'X Files Loaded!' message based on how many files are loaded
+        // Stuff to show the 'X Files Loaded!' message based on how many files are loaded
 
         private void browse_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 int files = 0;
-                foreach (string file in openFileDialog1.FileNames){files++;}
-                if (files == 1){browse.Text = files + " file loaded!";}
-                else{browse.Text = files + " files loaded!";}
+                foreach (string file in openFileDialog1.FileNames) { files++; }
+                if (files == 1) { browse.Text = files + " file loaded!"; }
+                else { browse.Text = files + " files loaded!"; }
                 save.Enabled = true;
             }
         }
 
-            // I'll make my own exit button, with blackjack, and overused jokes
+        // I'll make my own exit button, with blackjack, and overused jokes
 
-        private void exit_Click(object sender, EventArgs e){System.Windows.Forms.Application.Exit();}
+        private void exit_Click(object sender, EventArgs e) { System.Windows.Forms.Application.Exit(); }
 
-            // Gay Code Jail
+        // Gay Code Jail
 
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -121,12 +123,20 @@ namespace PhotoOrg
             timer1.Start();
         }
 
-            // For showing / hiding the job list
+        // For showing / hiding the job list
 
         private void mail_CheckedChanged(object sender, EventArgs e)
         {
-            if (mail.Checked){ PhotoOrg.ActiveForm.Width = 930; } else { PhotoOrg.ActiveForm.Width = 472; }
-            
+            if (mail.Checked) { PhotoOrg.ActiveForm.Width = 930; } else { PhotoOrg.ActiveForm.Width = 472; }
+
         }
+
+        // oopsie whoopsie looks like you cant remove these
+
+        private void textBox1_TextChanged(object sender, EventArgs e){}
+
+        private void textBox2_TextChanged(object sender, EventArgs e){}
+
+        private void openFileDialog1_FileOk(object sender, EventArgs e){}
     }
 }
